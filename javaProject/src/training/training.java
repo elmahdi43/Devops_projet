6db4f8f904bb8e789d30ecd6bc8b9c7f15815c54
El Mahdi Oukhamou <elmahdi43.software@gmail.com>
2021-12-16 10:26:53 +0100

    adding file

---
+++ javaProject/src/training/training.java
@@ -0,0 +1,17 @@
+package training;
+import java.util.Scanner;
+public class training {
+	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
+		System.out.print("frist number");
+		int num1= input.nextInt(); 
+		System.out.print("second number");
+		int num2= input.nextInt(); 
+		int somme= num1 +num2 ;
+		System.out.println();
+		System.out.println("result:"+ " "+ somme );
+		
+		
+	}
+
+}
